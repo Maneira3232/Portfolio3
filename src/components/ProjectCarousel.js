8185fb97f140b64c111410b81b474d5d6003e83b
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2022-05-23 11:46:44 -0400

    carousel adjustment

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -16,33 +16,33 @@ const ProjectCarousel = () => {
 
      <Carousel variant="dark" >
     <Carousel.Item className="carousel" interval={1000} >
-      <Image className="CarouselImage" width={1200} height={600} alt="900x500" src={PokeFinder}></Image><div className="carousel-caption">
+      <Image className="CarouselImage" width={1150} height={500} alt="900x500" src={PokeFinder}></Image><div className="carousel-caption">
         <p className="text">
-          <Button href="https://bchen41.github.io/PokeFinder/" variant="success">Click to View</Button>
+          <Button href="https://bchen41.github.io/PokeFinder/" variant="success">Explore Here</Button>
         </p>
       </div>
     </Carousel.Item>
     <Carousel.Item interval={1000}>
-      <Image className="CarouselImage" width={1200} height={600} alt="900x500" src={MrPassword}></Image>
+      <Image className="CarouselImage" width={1150} height={500} alt="900x500" src={MrPassword}></Image>
      <div className="carousel-caption">
         <p className="text">
-          <Button href="https://maneira3232.github.io/Mr.Pass-Word/" variant="success">Click to View</Button>
+          <Button href="https://maneira3232.github.io/Mr.Pass-Word/" variant="success">Explore Here </Button>
         </p>
       </div>
     </Carousel.Item>
     <Carousel.Item interval={1000}>
-      <Image className="CarouselImage" width={1200} height={600} alt="900x500" src={Hypo}></Image>
+      <Image className="CarouselImage" width={1150} height={500} alt="900x500" src={Hypo}></Image>
      <div className="carousel-caption">
       <p className="text">
-          <Button href="https://auction-pandemic-essentials.herokuapp.com/login" variant="success">Click to View</Button>
+          <Button href="https://auction-pandemic-essentials.herokuapp.com/login" variant="success">Explore Here</Button>
         </p>
       </div>
     </Carousel.Item>
     <Carousel.Item interval={1000}>
-      <Image className="CarouselImage" width={1200} height={600} alt="900x500" src={mix}></Image>
+      <Image className="CarouselImage" width={1150} height={500} alt="900x500" src={mix}></Image>
      <div className="carousel-caption">
       <p className="text">
-          <Button href="https://mixology-masters.herokuapp.com/" variant="success">Click to View</Button>
+          <Button href="https://mixology-masters.herokuapp.com/" variant="success">Explore Here</Button>
         </p>
       </div>
     </Carousel.Item>
