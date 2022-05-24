787c97377b703675f7ff07cfcab137eb0bdfc856
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2022-05-24 14:45:05 -0400

    Updates

---
+++ precache-manifest.6a5e31c93593dc06f9224de9471e6867.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec73f4eab4fb3219da72",
+    "url": "/Portfolio3/static/css/main.9a09de7a.chunk.css"
+  },
+  {
+    "revision": "ec73f4eab4fb3219da72",
+    "url": "/Portfolio3/static/js/main.ec73f4ea.chunk.js"
+  },
+  {
+    "revision": "b633ddec4110d79c4558",
+    "url": "/Portfolio3/static/css/1.90c7a7b5.chunk.css"
+  },
+  {
+    "revision": "b633ddec4110d79c4558",
+    "url": "/Portfolio3/static/js/1.b633ddec.chunk.js"
+  },
+  {
+    "revision": "960ed877c4f1d1bbb44f",
+    "url": "/Portfolio3/static/js/runtime~main.960ed877.js"
+  },
+  {
+    "revision": "ebcce4d9a121b0f3cfac594390396e81",
+    "url": "/Portfolio3/static/media/man.ebcce4d9.png"
+  },
+  {
+    "revision": "2302ee6dd201662d1d309d7bf8b3648f",
+    "url": "/Portfolio3/static/media/PokeFinder.2302ee6d.png"
+  },
+  {
+    "revision": "a8582398b002ef26c29952c7813a7cbb",
+    "url": "/Portfolio3/static/media/Hypo.a8582398.png"
+  },
+  {
+    "revision": "08335ff3dbb0c8b0e0e8d0b0543ac3f5",
+    "url": "/Portfolio3/static/media/Mixology-Masters.08335ff3.png"
+  },
+  {
+    "revision": "7e0bd9b884ca53072560f2ce19b99b0b",
+    "url": "/Portfolio3/static/media/mr.password.7e0bd9b8.png"
+  },
+  {
+    "revision": "925f11200fcee1d9ccc582c941046d85",
+    "url": "/Portfolio3/index.html"
+  }
+];
